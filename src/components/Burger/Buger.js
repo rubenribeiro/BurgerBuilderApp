7f868239ff06b8531a65c9ea0a1d7de3c7cc714c
Ruben Ribeiro <rubenribeiro@outlook.com>
2018-01-22 00:43:40 -0500

    Add dynamic ingredients

--- src/components/Burger/Buger.js
+++ src/components/Burger/Buger.js
@@ -3,13 +3,28 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.css';
 
-
 const burger = (props) => {
+
+    /*Transforming the ingredientst object "props" into an array:
+    the Object.keys(props.ingredients) returns an array containing 
+    the keys, i.e., the ingredient names. Using the map function on 
+    this array, we construct a new array using the Array() function 
+    by passing the number of times each ingredient must be added, 
+    (which is the value in the original ingredients object passed as props)
+     as the value. Then for each array of specific ingredients, 
+     we return the JSX containing the BurgerIngredient component.*/
+    
+     const trasformedIngredients = Object.keys(props.ingredients)
+                 .map(igKey => { 
+                     return [...Array(props.ingredients[igKey])].map((_, i) => {
+                          return <BurgerIngredient key={igKey + i} type={igKey} />
+                 });
+          });   
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {trasformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BugerBuilder/BurgerBuilder.js
+++ src/containers/BugerBuilder/BurgerBuilder.js
@@ -3,11 +3,25 @@ import Utility from '../../hoc/Utility';
 import Burger from '../../components/Burger/Buger'
 
 class BurgerBuilder extends Component {
+    //Alternative for using state
+    /*constructor(props){
+        super(props);
+        this.state = {"..."};        
+    }*/
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Utility>
-                <Burger />
-                <div>test</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Utility>
         );

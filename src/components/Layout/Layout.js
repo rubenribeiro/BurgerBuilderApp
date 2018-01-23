3d6950737d4a55fcc581ae250e844845514a07a1
Ruben Ribeiro <rubenribeiro@outlook.com>
2018-01-23 01:22:36 -0500

    initial sidebar configuration

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Utility from '../../hoc/Utility';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
 const layout = (props) => (
     <Utility>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
